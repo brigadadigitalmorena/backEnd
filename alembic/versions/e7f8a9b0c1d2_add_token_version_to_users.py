624763defbc99385193b5db53038a405f9c83043
PoloBustillo <39663418+PoloBustillo@users.noreply.github.com>
2026-02-18 12:48:50 -0600

    fix: replace duplicate migration ID a1b2c3d4e5f6 with e7f8a9b0c1d2 for token_version

--- alembic/versions/e7f8a9b0c1d2_add_token_version_to_users.py
+++ alembic/versions/e7f8a9b0c1d2_add_token_version_to_users.py
@@ -1,8 +1,8 @@
 """add token_version to users
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: e7f8a9b0c1d2
 Revises: 4d8aceb40133
-Create Date: 2025-01-01 00:00:00.000000
+Create Date: 2026-02-18 00:00:00.000000
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'a1b2c3d4e5f6'
+revision: str = 'e7f8a9b0c1d2'
 down_revision: Union[str, None] = '4d8aceb40133'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
